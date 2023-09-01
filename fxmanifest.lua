1c475bc394228b71701a5679dd1b9cc04eebecb6
TERHALANGKASTA <136704151+terhalangkasta@users.noreply.github.com>
2023-09-02 05:19:30 +0700

    Version Checker Synchronization

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'ZF Labo'
 description 'QB Menu & Inputs Converter for OX_LIB'
-version '1.0.0'
+version '1.1.0'
 
 client_script 'client/*.lua'
 server_script 'versioncheck.lua'
